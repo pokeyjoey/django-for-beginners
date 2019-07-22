71a1751e1f9a6c162ce7cd670fb16c7fc3ec4b64
Jeff Roche <jkeauctions@gmail.com>
2019-07-22 11:09:51 -0400

    forms and user accounts

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
 from .models import Post
 
@@ -8,7 +12,21 @@ class BlogListView(ListView):
     model = Post
     template_name='home.html'
 
-
 class BlogDetailView(DetailView):
     model = Post
     template_name='post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
